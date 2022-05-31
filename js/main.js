4b492576b7aa368bcf394ce37bad0a0f586bedc2
Andy Chen <chen.andy18@gmail.com>
2022-05-31 22:36:48 +0000

    completed adding a sibling div[data-view=entries] to the HTML. adding an empty unordered list to the div[data-view=entries] element in the HTML. adding some dummy journal entries to the unordered list in the HTML and style them to match the wireframe. Checked that the entries list looks good on both mobile and desktop screens. Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -27,3 +27,42 @@ function dataSet(event) {
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   event.target.reset();
 }
+
+// function createEntries(entry) {
+//   var $empty = document.createElement('div');
+//   var $divColHalf = document.createElement('div');
+//   $divColHalf.className = 'column-half';
+//   $empty.appendChild($divColHalf);
+//   var $unorder = document.createElement('ul');
+//   $divColHalf.appendChild($unorder);
+//   var $item = document.createElement('li');
+//   $unorder.appendChild($item);
+//   var $image = document.createElement('img');
+//   $image.setAttribute('src', entry.photoURL);
+//   $image.className = 'descriptions';
+//   $item.appendChild($image);
+//   var $divColHalf2 = document.createElement('div');
+//   $divColHalf2.className = 'column-half';
+//   $empty.appendChild($divColHalf2);
+//   var $unorder2 = document.createElement('ul');
+//   $divColHalf2.appendChild($unorder2);
+//   var $item2 = document.createElement('li');
+//   $unorder2.appendChild($item2);
+//   var $h2 = document.createElement('h2');
+//   $h2.textContent = entry.title;
+//   $item2.appendChild($h2);
+//   var $item3 = document.createElement('li');
+//   $unorder2.appendChild($item3);
+//   var $p = document.createElement('p');
+//   $p.className = 'descriptions';
+//   $p.textContent = entry.notes;
+//   $item3.appendChild($p);
+
+//   return $empty;
+// }
+
+// var entry1 = {
+//   photoURL: '/images/favicon.ico',
+//   title: 'something',
+//   notes: 'black and yellow'
+// };
