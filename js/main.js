c00661d19b10b87f2bb8ff7230bc6ad9508d381f
Andy Chen <chen.andy18@gmail.com>
2022-05-31 02:52:03 +0000

    completed Listen for 'submit' events on the journal entry form and:(6 sub checkmarks).

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-var imageURL = document.getElementById('img-URL');
-var image = document.querySelector('.placeholder-img');
+var $imageLink = document.getElementById('img-link');
+var $image = document.querySelector('.placeholder-img');
 
-imageURL.addEventListener('input', function (event) {
-  image.setAttribute('src', event.target.value);
+$imageLink.addEventListener('input', function (event) {
+  $image.setAttribute('src', event.target.value);
+});
+
+var $entry = document.getElementById('entry');
+var $title = document.getElementById('title');
+var $notes = document.getElementById('notes');
+
+$entry.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var singleData = {
+    title: $title.value,
+    photoURL: $imageLink.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId++
+  };
+  data.entries.unshift(singleData);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $title.value = '';
+  $imageLink.value = '';
+  $notes.value = '';
 });
